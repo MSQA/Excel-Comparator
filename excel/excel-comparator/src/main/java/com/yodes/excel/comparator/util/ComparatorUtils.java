b7d2805b5392cdc318e6e81159554eb946582890
Stephen Dillon <yodareloaded@gmail.com>
2013-02-23 23:21:48 +0000

    Adding tests

--- excel/excel-comparator/src/main/java/com/yodes/excel/comparator/util/ComparatorUtils.java
+++ excel/excel-comparator/src/main/java/com/yodes/excel/comparator/util/ComparatorUtils.java
@@ -1,51 +1,80 @@
 package com.yodes.excel.comparator.util;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.yodes.excel.comparator.comparators.HSSFComparator;
 import com.yodes.excel.comparator.model.ExcelRow;
 
 public class ComparatorUtils {
 
+	private static final Logger logger = LoggerFactory.getLogger(HSSFComparator.class);
+
+	/**
+	 * Method to convert a {@link HSSFRow} to a {@link ExcelRow}
+	 * 
+	 * @param row
+	 *            to convert
+	 * @return {@link ExcelRow} converted row
+	 */
 	public static ExcelRow convertToExcelRow(HSSFRow row) {
 		ExcelRow excelRow = new ExcelRow();
 		if (row != null) {
 			if (row.getLastCellNum() != -1) {
 				for (int j = 0; j < row.getLastCellNum(); j++) {
-					try {
-						if (row.getCell(j) != null) {
-							HSSFRichTextString orgionalString = row.getCell(j).getRichStringCellValue();
-							excelRow.getCells().add(orgionalString.toString());
-						} else {
-							excelRow.getCells().add(" ");
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+					convertHSSFCell(excelRow, row.getCell(j));
 				}
 			}
 		}
 		return excelRow;
 	}
 
+	protected static void convertHSSFCell(ExcelRow excelRow, HSSFCell cell) {
+		try {
+			if (cell != null) {
+				HSSFRichTextString orgionalString = cell.getRichStringCellValue();
+				excelRow.getCells().add(orgionalString.toString());
+			} else {
+				excelRow.getCells().add(" ");
+			}
+		} catch (Exception e) {
+			logger.warn("Error reading cell string value : " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Method to convert a {@link XSSFRow} to a {@link ExcelRow}
+	 * 
+	 * @param row
+	 *            to convert
+	 * @return {@link ExcelRow} converted row
+	 */
 	public static ExcelRow convertToExcelRow(XSSFRow row) {
 		ExcelRow excelRow = new ExcelRow();
 		if (row != null) {
 			if (row.getLastCellNum() != -1) {
 				for (int j = 0; j < row.getLastCellNum(); j++) {
-					try {
-						if (row.getCell(j) != null) {
-							excelRow.getCells().add(row.getCell(j).getStringCellValue());
-						} else {
-							excelRow.getCells().add(" ");
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+					convertXSSFCell(excelRow, row.getCell(j));
 				}
 			}
 		}
 		return excelRow;
+	}
+
+	protected static void convertXSSFCell(ExcelRow excelRow, XSSFCell cell) {
+		try {
+			if (cell != null) {
+				excelRow.getCells().add(cell.getStringCellValue());
+			} else {
+				excelRow.getCells().add(" ");
+			}
+		} catch (Exception e) {
+			logger.warn("Error reading cell string value : " + e.getMessage());
+		}
 	}
 }
